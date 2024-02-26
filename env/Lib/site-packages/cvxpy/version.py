6a5398e6db400e633a84dd2d3cae0fd10123bebd
Oyaabuun <113195257+Oyaabuun@users.noreply.github.com>
2024-02-26 15:08:05 +0530

    first commit afdasfasf as ffas fasd

---
+++ env/Lib/site-packages/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.4.2'
+version = '1.4.2'
+full_version = '1.4.2'
+git_revision = '5d1416b'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
---
+++ ok.py
@@ -0,0 +1,13 @@
+'''
+to create a virtual environmetn in python use 
+# On Windows
+python -m venv venv
+
+To activate it use 
+.\env\Scripts\activate
+
+to deactivate it use
+deactivate
+
+
+'''
